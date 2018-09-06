fe1e10f1d248ba8fe4434a16bc1ebc54c7344354
Lucas <lucass.hauptmann@gmail.com>
2018-09-06 19:31:34 -0300

    Uploading project from local area to github

---
+++ src/Root.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import async from 'middlewares/async';
+import stateValidator from 'middlewares/stateValidator';
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import reducers from 'reducers';
+
+export default ({ children, initialState={} }) => {
+    const store = createStore(
+        reducers,
+        initialState,
+        applyMiddleware(async, stateValidator)
+    );
+
+    return  <Provider store={store}>{children}</Provider>
+};
